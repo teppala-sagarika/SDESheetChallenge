1dfa39d0aaf86cb703f694410e9fd8559ec274f7
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-01 17:21:21 +0530

    [LeetCode] Next Permutation · Time: O(nlogn) | Space: O(1) · Lang: cpp · Note: Arrays

--- leetcode/Next_Permutation.cpp
+++ leetcode/Next_Permutation.cpp
@@ -1,18 +1,37 @@
 // ────────────────────────────────────────────────────────────
 // Problem : Next Permutation
 // Platform: LeetCode
-// URL     : https://leetcode.com/problems/next-permutation/submissions/2018833885/
+// URL     : https://leetcode.com/problems/next-permutation/
 // Language: cpp
-// Date    : 6/1/2026, 12:40:14 PM
+// Date    : 6/1/2026, 5:19:48 PM
 // ────────────────────────────────────────────────────────────
-// Time Complexity : O(n)
+// Time Complexity : O(nlogn)
 // Space Complexity: O(1)
-// Notes           : arrays ( builtin func )
+// Notes           : Arrays
 // ────────────────────────────────────────────────────────────
 
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        int breakPt=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                breakPt=i;
+                break;
+            }
+        }
+        if(breakPt==-1){
+            sort(nums.begin(),nums.end());
+        }
+        else{
+            for(int i=n-1;i>breakPt;i--){
+                if(nums[i]>nums[breakPt]){
+                    swap(nums[i],nums[breakPt]);
+                    break;
+                }
+            }
+            sort(nums.begin()+breakPt+1,nums.end());
+        }
     }
 };
