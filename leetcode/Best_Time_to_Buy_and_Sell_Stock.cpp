e67c49bd0386e1001a09073d205ef16432e4f3dc
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-02 17:17:23 +0530

    [LeetCode] Best Time to Buy and Sell Stock · Time: O(??) | Space: O(??) · Lang: cpp

---
+++ leetcode/Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,28 @@
+// ────────────────────────────────────────────────────────────
+// Problem : Best Time to Buy and Sell Stock
+// Platform: LeetCode
+// URL     : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/2020133387/
+// Language: cpp
+// Date    : 6/2/2026, 5:17:11 PM
+// ────────────────────────────────────────────────────────────
+// Time Complexity : O(??)
+// Space Complexity: O(??)
+// ────────────────────────────────────────────────────────────
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int maxi=0;
+        int n=prices.size();
+        for(int i=1;i<n;i++){
+            if(prices[i]<mini){
+                mini=prices[i];
+            }
+            else{
+                maxi=max(maxi,prices[i]-mini);
+            }
+        }
+        return maxi;
+    }
+};
