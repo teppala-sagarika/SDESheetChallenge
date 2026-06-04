964a314d6a2567d255a4ae05e4551a30ded81f81
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-04 13:10:46 +0530

    Add findDuplicate function in Find_the_Duplicate_Number_better.cpp
    Implement a better solution to find the duplicate number using a set.

---
+++ leetcode/Find_the_Duplicate_Number_better.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(st.find(nums[i])!=st.end()){
+                return nums[i];
+            }
+            st.insert(nums[i]);
+        }
+        return -1;
+    }
+};
+
+//T.C = O(n)
+//S.C = O(n)
