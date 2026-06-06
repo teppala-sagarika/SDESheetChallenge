3b3d359093a8ec019ffb3c925c033afe4b8a35d3
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-06 12:42:39 +0530

    Add Unique_Paths_Memorization.cpp solution
    Implement a solution for the Unique Paths problem using memoization.

---
+++ leetcode/Unique_Paths_Memorization.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int solve(int i,int j ,int m , int n,vector<vector<int>>& dp){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(i>=m || j>=n){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int down=solve(i+1,j,m,n,dp);
+        int ryt=solve(i,j+1,m,n,dp);
+        return dp[i][j]=down + ryt;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(0,0,m,n,dp);
+    }
+};
+
+//T.C = O(mxn)
+//S.C = O(mxn) + O(m+n)
