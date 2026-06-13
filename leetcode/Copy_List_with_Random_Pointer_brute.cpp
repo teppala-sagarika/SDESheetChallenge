fe8898bb6b41149b9a5163c0836c2e742c02c065
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-13 12:15:55 +0530

    Implement brute force solution for random pointer list

---
+++ leetcode/Copy_List_with_Random_Pointer_brute.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        unordered_map<Node*,Node*> mp;
+        Node* temp=head;
+        while(temp!=nullptr){
+            Node* newNode=new Node(temp->val);
+            mp[temp]=newNode;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=nullptr){
+            mp[temp]->next=mp[temp->next];
+            mp[temp]->random=mp[temp->random];
+            temp=temp->next;
+        }
+        return mp[head];
+    }
+};
+
+//T.C = O(n)
+//S.C =O(n)
