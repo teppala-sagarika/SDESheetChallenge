7951ad44a795952bfb6dde32936723c5c757a612
Teppala Sagarika <sagarikateppala@gmail.com>
2026-06-14 13:28:39 +0530

    [LeetCode] Trapping Rain Water · Time: O(n) | Space: O(1) · Lang: cpp · Note: arrays

--- leetcode/Trapping_Rain_Water.cpp
+++ leetcode/Trapping_Rain_Water.cpp
@@ -1,12 +1,12 @@
 // ────────────────────────────────────────────────────────────
 // Problem : Trapping Rain Water
 // Platform: LeetCode
-// URL     : https://leetcode.com/problems/trapping-rain-water/submissions/2032563207/
+// URL     : https://leetcode.com/problems/trapping-rain-water/submissions/2032589447/
 // Language: cpp
-// Date    : 6/14/2026, 12:55:20 PM
+// Date    : 6/14/2026, 1:26:37 PM
 // ────────────────────────────────────────────────────────────
 // Time Complexity : O(n)
-// Space Complexity: O(n)
+// Space Complexity: O(1)
 // Notes           : arrays
 // ────────────────────────────────────────────────────────────
 
@@ -14,17 +14,28 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n=height.size();
-        vector<int> left(n),ryt(n);
-        int l=0,r=0;
-        for(int i=0;i<n;i++){
-            l=left[i]=max(l,height[i]);
-            r=ryt[n-i-1]=max(r,height[n-1-i]);
-        }
+        int l=0,r=n-1;
+        int lmx=0,rmx=0;
         int ans=0;
-        for(int i=0;i<n;i++){
-            int mini=min(left[i],ryt[i]);
-            ans+=mini-height[i];
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(lmx>height[l])
+                ans+=lmx-height[l];
+                else
+                lmx=max(lmx,height[l]);
+                l++;
+            }
+            else{
+                if(rmx>height[r])
+                ans+=rmx-height[r];
+                else
+                rmx=max(rmx,height[r]);
+                r--;
+            }
         }
         return ans;
     }
 };
+
+//T.C = O(n)
+//S.C = O(1)
