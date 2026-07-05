ed97aea6332b655ffac266ec25c6b02f44ccec26
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-05 19:45:35 +0530

    Add Binary Tree Postorder Traversal with 2 stacks
    Implement postorder traversal using two stacks.

---
+++ leetcode/Binary_Tree_Postorder_Traversal_2stack.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        //2 stacks
+        vector<int> ans;
+        if(root==nullptr){
+            return ans;
+        }
+        stack<TreeNode*> st1,st2;
+        TreeNode* node=root;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* node=st1.top();
+            st1.pop();
+            st2.push(node);
+            if(node->left)
+            st1.push(node->left);
+            if(node->right)
+            st1.push(node->right);
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
+};
+
+//T.C = O(n)
+//S.C = O(h)
