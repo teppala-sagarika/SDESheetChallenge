2b17840e2539637728bace621d9442120ef1b3f9
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-07 20:02:06 +0530

    Add Morris traversal for binary tree preorder
    Implement Morris traversal for binary tree preorder.

---
+++ leetcode/Binary_Tree_Preorder_Traversal_Morris.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+//MORRIS TRAVERSAL - THREADED BINARY TREE :)
+    vector<int> preorderTraversal(TreeNode* root) {
+        TreeNode* cur=root;
+        vector<int> ans;
+        while(cur!=nullptr){
+            if(cur->left==nullptr){
+                //cur is root 
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){//go till atmost right
+                      prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    //add a thread
+                    prev->right=cur;        
+                    ans.push_back(cur->val);
+                    cur=cur->left;
+                }else{
+                    //already thread exists
+                    prev->right=nullptr;
+                    cur=cur->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//T.C = O(2n)
+//S.C = O(1)
