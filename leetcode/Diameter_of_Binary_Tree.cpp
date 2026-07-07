e9c5b184b5f529630740075e77b76ef6ff19b2a2
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-07 20:41:45 +0530

    Add Diameter_of_Binary_Tree solution
    Implement a solution to calculate the diameter of a binary tree.

---
+++ leetcode/Diameter_of_Binary_Tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int solve(TreeNode* node,int &dia){//height
+    if(!node){
+        return 0;
+    }
+    int lh=solve(node->left,dia);
+    int rh=solve(node->right,dia);
+    dia=max(dia,lh+rh);
+    return 1+max(lh,rh);
+}
+    int diameterOfBinaryTree(TreeNode* root) {
+        int dia=0;
+        solve(root,dia);
+        return dia;
+    }
+};
+
+//T.C = O(n)
+//S.C = O(h)
