3b8826554b1efd0cf08d64e227ccc6e4fc6daaa3
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-16 12:01:22 +0530

    [LeetCode] Kth Largest Element in an Array · Time: O(n^2) | Space: O(1) · Lang: cpp · Note: Quick Select - Hoare's Partition

--- leetcode/Kth_Largest_Element_in_an_Array.cpp
+++ leetcode/Kth_Largest_Element_in_an_Array.cpp
@@ -1,24 +1,51 @@
 // ────────────────────────────────────────────────────────────
 // Problem : Kth Largest Element in an Array
 // Platform: LeetCode
-// URL     : https://leetcode.com/problems/kth-largest-element-in-an-array/submissions/2069384953/
+// URL     : https://leetcode.com/problems/kth-largest-element-in-an-array/description/
 // Language: cpp
-// Date    : 7/16/2026, 10:55:47 AM
+// Date    : 7/16/2026, 12:00:22 PM
 // ────────────────────────────────────────────────────────────
-// Time Complexity : O(n log k)
-// Space Complexity: O(k)
+// Time Complexity : O(n^2)
+// Space Complexity: O(1)
+// Notes           : Quick Select - Hoare's Partition
 // ────────────────────────────────────────────────────────────
 
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int,vector<int>,greater<int>> min_hp;
-        for(int i=0;i<nums.size();i++){
-            min_hp.push(nums[i]);
-            if(min_hp.size()>k){
-                min_hp.pop();
+    int quickSelectAlgo(vector<int>& nums,int l,int r){
+        int pivot=nums[l];
+        int i=l+1;
+        int j=r;
+        while(i<=j){
+            if(nums[i]<pivot && pivot<nums[j]){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+            if(nums[i]>=pivot){
+                i++;
+            }
+            if(nums[j]<=pivot){
+                j--;
             }
         }
-        return min_hp.top();
+        swap(nums[l],nums[j]);
+        return j;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        int pivotidx=0;
+        int l=0;
+        int r=nums.size()-1;
+        while(true){
+            pivotidx=quickSelectAlgo(nums,l,r);
+            if(pivotidx==k-1){
+                return nums[pivotidx];
+            }else if(pivotidx>k-1){
+                r=pivotidx-1;
+            }else{
+                l=pivotidx+1;
+            }
+        }
+        return -1;
     }
 };
