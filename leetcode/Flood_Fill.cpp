737cf660b3edfa8e13e694cc3ed1658278a8a9bb
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-16 12:58:38 +0530

    [LeetCode] Flood Fill · Time: O(N × M) | Space: O(N × M) · Lang: cpp · Note: bfs

--- leetcode/Flood_Fill.cpp
+++ leetcode/Flood_Fill.cpp
@@ -3,34 +3,45 @@
 // Platform: LeetCode
 // URL     : https://leetcode.com/problems/flood-fill/description/
 // Language: cpp
-// Date    : 7/16/2026, 12:32:23 PM
+// Date    : 7/16/2026, 12:57:50 PM
 // ────────────────────────────────────────────────────────────
-// Time Complexity : O(n*m)
-// Space Complexity: O(n*m)
-// Notes           : dfs
+// Time Complexity : O(N × M)
+// Space Complexity: O(N × M)
+// Notes           : bfs
 // ────────────────────────────────────────────────────────────
 
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image,int n,int m,int r, int c,int initColor, int newColor){
-        if(r<0 || c<0 || r>=n || c>=m){
-            return;
-        }
-        if(image[r][c]!=initColor){
-            return;
-        }
-        image[r][c]=newColor;
-        int row[]={1,-1,0,0};
-        int col[]={0,0,1,-1};
-        for(int i=0;i<4;i++){
-            dfs(image,n,m,r+row[i],c+col[i],initColor,newColor);
-        }
-    }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color){
+            return image;
+        }
+        
         int n=image.size();
         int m=image[0].size();
-        if(image[sr][sc]==color) return image;
-        dfs(image,n,m,sr,sc,image[sr][sc],color);
+        int initColor=image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc]=color;
+
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+
+            q.pop();
+
+            int row[]={1,-1,0,0};
+            int col[]={0,0,1,-1};
+
+            for(int i=0;i<4;i++){
+                int nr=r+row[i];
+                int nc=c+col[i];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==initColor){
+                    image[nr][nc]=color;
+                    q.push({nr,nc});
+                }
+            }
+        }
         return image;
     }
 };
