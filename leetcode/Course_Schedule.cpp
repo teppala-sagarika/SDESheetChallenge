0eea73a89a64ac77cff3d3224b37262f52fae3ae
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-18 16:17:51 +0530

    [LeetCode] Course Schedule · Time: O(V + E) | Space: O(V + E) · Lang: cpp · Note: using topological sort - BFS (only applicable for DAGs)

--- leetcode/Course_Schedule.cpp
+++ leetcode/Course_Schedule.cpp
@@ -1,50 +1,49 @@
 // ────────────────────────────────────────────────────────────
 // Problem : Course Schedule
 // Platform: LeetCode
-// URL     : https://leetcode.com/problems/course-schedule/submissions/2071888512/
+// URL     : https://leetcode.com/problems/course-schedule/submissions/2072109879/
 // Language: cpp
-// Date    : 7/18/2026, 12:22:47 PM
+// Date    : 7/18/2026, 4:16:26 PM
 // ────────────────────────────────────────────────────────────
-// Time Complexity : O(E) + O(V + E)
+// Time Complexity : O(V + E)
 // Space Complexity: O(V + E)
-// Notes           : detect cycle using dfs (2 visited arrays)
+// Notes           : using topological sort - BFS (only applicable for DAGs)
 // ────────────────────────────────────────────────────────────
 
 class Solution {
 public:
-    bool dfsCheckCycle(int i,vector<vector<int>> &adjlst,vector<int> &vis,vector<int>& dfsVis){
-        vis[i]=1;
-        dfsVis[i]=1;
-        for(int it:adjlst[i]){
-            if(!vis[it]){
-                if(dfsCheckCycle(it,adjlst,vis,dfsVis)==true){
-                    return true;
-                }
-            }else{
-                if(dfsVis[it]){
-                    return true;
-                }
-            }
-        }
-        dfsVis[i]=0;
-        return false;
-    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> vis(numCourses,0),dfsVis(numCourses,0);
+        vector<int> inorder(numCourses,0);
         vector<vector<int>> adjlst(numCourses);
         int n=prerequisites.size();
         for(int i=0;i<n;i++){
             int a=prerequisites[i][0];
             int b=prerequisites[i][1];
+            inorder[a]++;
             adjlst[b].push_back(a);
         }
+        queue<int> q;
         for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                if(dfsCheckCycle(i,adjlst,vis,dfsVis)==true){
-                    return false;
+            if(inorder[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topoSort;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topoSort.push_back(node);
+            for(int it:adjlst[node]){
+                inorder[it]--;
+                if(inorder[it]==0){
+                    q.push(it);
                 }
             }
         }
-        return true;
+        if(topoSort.size()==numCourses){
+            //no cycle - DAG 
+            return true;
+        }
+        return false;
     }
 };
