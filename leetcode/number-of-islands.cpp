3ae26d2dfe791992127e91e6db44b252aa813800
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-22 23:59:15 +0530

    [LeetCode] number-of-islands · Time: O(m*n) | Space: O(m*n) · Lang: C++ · Note: bfs

--- leetcode/number-of-islands.cpp
+++ leetcode/number-of-islands.cpp
@@ -3,26 +3,33 @@
 // Platform: LeetCode
 // URL     : https://leetcode.com/problems/number-of-islands/
 // Language: C++
-// Date    : 7/22/2026, 11:38:27 PM
+// Date    : 7/22/2026, 11:58:52 PM
 // ────────────────────────────────────────────────────────────
 // Time Complexity : O(m*n)
 // Space Complexity: O(m*n)
-// Notes           : dfs
+// Notes           : bfs
 // ────────────────────────────────────────────────────────────
 
 class Solution {
 private:
-    void dfs(int r,int c,int m,int n,vector<vector<int>> &vis,vector<vector<char>>& grid){
-        if(r<0 || c<0 || r>=m || c>=n || grid[r][c]=='0' ||vis[r][c]==1){
-            return;
+    void bfs(int row,int col,int m,int n,vector<vector<int>> &vis,vector<vector<char>>& grid){
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        vis[row][col]=1;
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            int delRow[] = {-1, 1, 0, 0};
+            int delCol[] = {0, 0, -1, 1};
+            for (int i = 0; i < 4; i++) {
+            int nr = r + delRow[i];
+            int nc = c + delCol[i];
+            if(nr >= 0 && nc >= 0 && nr < m && nc < n && grid[nr][nc] == '1' && vis[nr][nc] == 0) {
+            q.push({nr, nc});
+            vis[nr][nc] = 1;
+            }
         }
-        vis[r][c]=1;
-        int dr[]={1,-1,0,0};
-        int dc[]={0,0,1,-1};
-        for(int i=0;i<4;i++){
-            int nr=r+dr[i];
-            int nc=c+dc[i];
-            dfs(nr,nc,m,n,vis,grid);
         }
     }
 public:
@@ -34,7 +41,7 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]=='1' && vis[i][j]==0){
-                    dfs(i,j,m,n,vis,grid);
+                    bfs(i,j,m,n,vis,grid);
                     c++;
                 }
             }
