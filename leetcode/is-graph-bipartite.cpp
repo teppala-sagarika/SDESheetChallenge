bedfa2391fdb5f40329194f0e7c8f5968cdf3f28
Teppala Sagarika <sagarikateppala@gmail.com>
2026-07-23 22:38:16 +0530

    [LeetCode] is-graph-bipartite · Time: O(V + E) | Space: O(V) · Lang: C++

--- leetcode/is-graph-bipartite.cpp
+++ leetcode/is-graph-bipartite.cpp
@@ -3,37 +3,38 @@
 // Platform: LeetCode
 // URL     : https://leetcode.com/problems/is-graph-bipartite/
 // Language: C++
-// Date    : 7/23/2026, 10:31:54 PM
+// Date    : 7/23/2026, 10:37:31 PM
 // ────────────────────────────────────────────────────────────
 // Time Complexity : O(V + E)
 // Space Complexity: O(V)
-// Notes           : dfs
 // ────────────────────────────────────────────────────────────
 
 class Solution {
-private:
-    bool dfs(int i,int col,vector<int>& color,vector<vector<int>>& graph){
-        color[i]=col;
-        for(auto it:graph[i]){
-            if(color[it]==-1){
-                if(!dfs(it,!col,color,graph)) return false;
-            }else if(color[it]==col){
-                return false;
-            }
-        }
-        return true;
-    }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int> color(n,-1);
-        for(int i=0;i<n;i++){
-            if(color[i]==-1){
-                if(!dfs(i,0,color,graph)){
-                    return false;
+        int n = graph.size();
+        vector<int> color(n, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] != -1)
+                continue;
+            queue<int> q;
+            q.push(i);
+            color[i] = 0;
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for (int neigh : graph[node]) {
+                    if(color[neigh]==-1){
+                        color[neigh]=!color[node];
+                        q.push(neigh);
+                    }
+                    else if (color[neigh]==color[node]) {
+                        return false;
+                    }
                 }
             }
         }
+
         return true;
     }
 };
